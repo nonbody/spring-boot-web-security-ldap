59e6dbc1b3a37f9e612ba3dd39339f04922cb578
non.t <nonbody.en.th@hotmail.com>
2018-02-05 14:57:30 +0700

    first commit

---
+++ src/main/java/com/web/springbootwebsecurity/SpringBootWebSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.web.springbootwebsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSecurityApplication.class, args);
+	}
+}
